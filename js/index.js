e1f37a77b6a05fbce36e69e0cde39d618b3fafbb
lakshmankotiki <kotiki.lakshman1996@gmail.com>
2019-12-28 18:13:44 +0530

    refarctored the code for better understanding

--- js/index.js
+++ js/index.js
@@ -1,35 +1,26 @@
-/** this function generates two random numbers
- *  and assign those randome numbers to two dices
- *  by using DOM, this function change the image as per dices number
- *  and display which player is the winner of this dice game
- */
-var diceWinner = function() {
+var randomNumber1 = Math.round((Math.random() * 6) + 1); // random number b/w 1 to 6
 
-    //Generates random numbers between 1 to 6
-    var dice1 = Math.round((Math.random() * 5) + 1);
-    var dice2 = Math.round((Math.random() * 5) + 1);
+var randomImage1 = "dice" + randomNumber1 + ".png"; //assiging rand num to the image
 
-    //by using DOM, Assign images as per dices number
-    dice1 === 1 ? document.getElementsByTagName("img")[0].setAttribute("src", "images/dice1.png") : dice1 === 2 ?
-    document.getElementsByTagName("img")[0].setAttribute("src", "images/dice2.png") : dice1 === 3 ?
-    document.getElementsByTagName("img")[0].setAttribute("src", "images/dice3.png") : dice1 === 4 ?
-    document.getElementsByTagName("img")[0].setAttribute("src", "images/dice4.png") : dice1 === 5 ?
-    document.getElementsByTagName("img")[0].setAttribute("src", "images/dice5.png") : dice1 === 6 ?
-    document.getElementsByTagName("img")[0].setAttribute("src", "images/dice6.png") : "";
+var randomSource1 = "images/" + randomImage1; //updating source
+//setting the random image source using DOM
+document.querySelectorAll("img")[0].setAttribute("src", randomSource1);
 
-    dice2 === 1 ? document.getElementsByTagName("img")[1].setAttribute("src", "images/dice1.png") : dice2 === 2 ?
-    document.getElementsByTagName("img")[1].setAttribute("src", "images/dice2.png") : dice2 === 3 ?
-    document.getElementsByTagName("img")[1].setAttribute("src", "images/dice3.png") : dice2 === 4 ?
-    document.getElementsByTagName("img")[1].setAttribute("src", "images/dice4.png") : dice2 === 5 ?
-    document.getElementsByTagName("img")[1].setAttribute("src", "images/dice5.png") : dice2 === 6 ?
-    document.getElementsByTagName("img")[1].setAttribute("src", "images/dice6.png") : "";
 
-    //show the winner as per highest number
-    if(dice1 === dice2) {
-        document.getElementsByTagName("h1")[0].textContent = "Hurray!......Draw";
-    }else if(dice1 > dice2) {
-        document.getElementsByTagName("h1")[0].textContent = "Player 1 is the winner";
-    } else {
-        document.getElementsByTagName("h1")[0].textContent = "Player 2 is the winner";
-    }
-};
+
+var randomNumber2 = Math.round((Math.random() * 6) + 1);
+
+var randomImage2 = "dice" + randomNumber2 + ".png";
+
+var randomSource2 = "images/" + randomImage2;
+
+document.querySelectorAll("img")[1].setAttribute("src", randomSource2);
+
+//Deciding winner logic and upating winner as the title
+if(randomNumber1 === randomNumber2) {
+    document.querySelector("h1").innerHTML = "Hurray....Draw!";
+}else if(randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "✌️ Player 1 wins!";
+} else {
+    document.querySelector("h1").innerHTML = "Player 2 wins ✌️!";
+}
